a5c711f706d10b2319fa2726a98f6145cf5d5399
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-11 16:29:48 +0900

    Update start_distccd.sh

--- app/start_distccd.sh
+++ app/start_distccd.sh
@@ -8,11 +8,13 @@ DISTCCD_LOG_FILE=/usr/local/apache2/htdocs/auth/distccd_log.txt
 touch ${DISTCCD_LOG_FILE}
 chmod 666 ${DISTCCD_LOG_FILE}
 
-# /usr/bin/distccd --port=3632 --listen=127.0.0.1 --user=nobody --jobs=1 --log-level=debug --log-file=${DISTCCD_LOG_FILE} --daemon --stats --stats-port=3633 --allow-private
-/usr/bin/distccd --port=3632 --listen=127.0.0.1 --user=nobody --jobs=1 --log-level=debug --log-stderr --daemon --stats --stats-port=3633 --allow-private
+# /usr/bin/distccd --port=3632 --listen=127.0.0.1 --user=nobody --jobs=1 --log-level=debug --log-stderr --daemon --stats --stats-port=3633 --allow-private --job-lifetime=180 --nice=10
+/usr/bin/distccd --port=3632 --listen=127.0.0.1 --user=nobody --jobs=1 --log-level=debug --log-file=${DISTCCD_LOG_FILE} --daemon --stats --stats-port=3633 --allow-private --job-lifetime=180 --nice=10
 
 sleep 10s
 ss -anpt
+
+curl -v http://127.0.0.1:3633/ &
 
 curl -sSLO https://github.com/nwtgck/go-piping-duplex/releases/download/v0.3.0-release-trigger2/piping-duplex-0.3.0-release-trigger2-linux-amd64.tar.gz
 tar xf piping-duplex-0.3.0-release-trigger2-linux-amd64.tar.gz
