d94be84cfbe34dd89dc4112465417ca7c957a9c7
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-11 13:40:02 +0900

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -3,7 +3,7 @@
 set -x
 
 curl -sSL -H 'Cache-Control: no-cache' -o /app/start_distccd.sh https://raw.githubusercontent.com/tshr20180821/render-10/main/app/start_distccd.sh?$(date +%s)
-cat /app/start_distccd.sh
+cat -n /app/start_distccd.sh
 chmod +x /app/start_distccd.sh
 # sleep 10s && /app/start_distccd.sh &
 
@@ -15,9 +15,9 @@ htpasswd -c -b /usr/local/apache2/htdocs/.htpasswd "${BASIC_USER}" "${BASIC_PASS
 chmod 644 /usr/local/apache2/htdocs/.htpasswd
 
 curl -sSL -H 'Cache-Control: no-cache' -o /app/apache2.conf https://raw.githubusercontent.com/tshr20180821/render-10/main/app/apache2.conf?$(date +%s)
-cat /app/apache2.conf
+cat -n /app/apache2.conf
 
 curl -sSL -H 'Cache-Control: no-cache' -o /app/apache2_LoadModule.conf https://raw.githubusercontent.com/tshr20180821/render-10/main/app/apache2_LoadModule.conf?$(date +%s)
-cat /app/apache2_LoadModule.conf
+cat -n /app/apache2_LoadModule.conf
 
 apachectl -f /app/apache2.conf -DFOREGROUND
