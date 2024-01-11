d5934ef57db7f9be39d349be5f9241c9106bdab7
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-11 09:25:25 +0900

    Update start_pre.sh

--- app/start_pre.sh
+++ app/start_pre.sh
@@ -5,7 +5,6 @@ set -x
 apt-get -qq update
 apt-get -q install -y curl iproute2 >/dev/null
 
-rm /app/start.sh
 curl -sSL -H 'Cache-Control: no-cache' -o /app/start.sh https://raw.githubusercontent.com/tshr20180821/render-10/main/app/start.sh?$(date +%s)
 cat /app/start.sh
 chmod +x /app/start.sh
