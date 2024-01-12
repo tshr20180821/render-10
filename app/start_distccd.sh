40e72d22bab7ddae20ab2d3be0fb7b36930181f8
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-13 06:14:02 +0900

    Update start_distccd.sh

--- app/start_distccd.sh
+++ app/start_distccd.sh
@@ -16,7 +16,7 @@ touch ${DISTCCD_LOG_FILE}
 chmod 666 ${DISTCCD_LOG_FILE}
 
 # /usr/bin/distccd --port=3632 --listen=127.0.0.1 --user=nobody --jobs=1 --log-level=debug --log-stderr --daemon --stats --stats-port=3633 --allow-private --job-lifetime=180 --nice=10
-/usr/bin/distccd --port=3632 --listen=127.0.0.1 --user=nobody --jobs=1 --log-level=debug --log-file=${DISTCCD_LOG_FILE} --daemon --stats --stats-port=3633 --allow-private --job-lifetime=180 --nice=10
+/usr/bin/distccd --port=3632 --listen=127.0.0.1 --user=nobody --jobs=4 --log-level=debug --log-file=${DISTCCD_LOG_FILE} --daemon --stats --stats-port=3633 --allow-private --job-lifetime=180 --nice=10
 
 sleep 10s
 ss -anpt
