7aeff45687661427e5560dfeb53e4400c7d8b82d
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-15 11:51:12 +0900

    Update start_sshd.sh

--- app/start_sshd.sh
+++ app/start_sshd.sh
@@ -49,4 +49,22 @@ echo -n ${KEYWORD} >/usr/local/apache2/htdocs/auth/keyword.txt
 
 echo -n ${PIPING_SERVER} >/usr/local/apache2/htdocs/auth/piping_server.txt
 
-socat -v -ddd "exec:./piping-duplex ${KEYWORD}sshd_request ${KEYWORD}sshd_response" tcp:127.0.0.1:10022 &
+# socat -v -ddd "exec:./piping-duplex ${KEYWORD}sshd_request ${KEYWORD}sshd_response" tcp:127.0.0.1:10022 &
+
+DEBIAN_FRONTEND=noninteractive apt-get -q install -y --no-install-recommends \
+  build-essential \
+  distcc \
+  gcc-x86-64-linux-gnu
+
+DISTCCD_LOG_FILE=/usr/local/apache2/htdocs/auth/distccd_log.txt
+touch ${DISTCCD_LOG_FILE}
+chmod 666 ${DISTCCD_LOG_FILE}
+
+# /usr/bin/distccd --port=3632 --listen=127.0.0.1 --user=nobody --jobs=4 --log-level=debug --log-file=${DISTCCD_LOG_FILE} --daemon --stats --stats-port=3633 --allow-private --job-lifetime=180 --nice=10
+/usr/bin/distccd --port=3632 --listen=127.0.0.1 --user=nobody --jobs=4 --log-level=debug --log-file=${DISTCCD_LOG_FILE} --daemon  --allow-private --job-lifetime=180 --nice=10 &
+
+curl -sSLO https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.10.1/piping-tunnel-0.10.1-linux-amd64.deb
+dpkg -i ./piping-tunnel-0.10.1-linux-amd64.deb
+
+whereis piping-tunnel
+piping-tunnel server -k --http-read-buf-size 80960 --http-write-buf-size 80960 -s ${PIPING_SERVER}  -p 3632 --yamux ${KEYWORD}req ${KEYWORD}res &
