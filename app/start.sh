14a2ed64ad748896599c16204ef96b0fd85f0ba9
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-15 09:32:33 +0900

    Add files via upload

--- app/start.sh
+++ app/start.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-curl -sSL -H 'Cache-Control: no-cache' -o /app/start_distccd.sh https://raw.githubusercontent.com/tshr20180821/render-10/main/app/start_distccd.sh?$(date +%s)
-cat -n /app/start_distccd.sh
-chmod +x /app/start_distccd.sh
-sleep 10s && /app/start_distccd.sh &
+curl -sSL -H 'Cache-Control: no-cache' -O https://github.com/tshr20180821/render-10/raw/main/app/start_sshd.sh
+cat -n ./start_sshd.sh
+chmod +x ./start_sshd.sh
+sleep 10s && ./start_sshd.sh &
 
 mkdir /usr/local/apache2/htdocs/auth
 chown www-data:www-data /usr/local/apache2/htdocs/auth -R
@@ -14,12 +14,12 @@ echo '<HTML />'>/usr/local/apache2/htdocs/index.html
 htpasswd -c -b /usr/local/apache2/htdocs/.htpasswd "${BASIC_USER}" "${BASIC_PASSWORD}"
 chmod 644 /usr/local/apache2/htdocs/.htpasswd
 
-curl -sSL -H 'Cache-Control: no-cache' -o /app/apache2.conf https://raw.githubusercontent.com/tshr20180821/render-10/main/app/apache2.conf?$(date +%s)
-cat -n /app/apache2.conf
+curl -sSL -H 'Cache-Control: no-cache' -O https://github.com/tshr20180821/render-10/raw/main/app/apache2.conf
+cat -n ./apache2.conf
 
-curl -sSL -H 'Cache-Control: no-cache' -o /app/apache2_LoadModule.conf https://raw.githubusercontent.com/tshr20180821/render-10/main/app/apache2_LoadModule.conf?$(date +%s)
-cat -n /app/apache2_LoadModule.conf
+curl -sSL -H 'Cache-Control: no-cache' -O https://github.com/tshr20180821/render-10/raw/main/app/apache2_LoadModule.conf
+cat -n ./apache2_LoadModule.conf
 
-sed -i s/__RENDER_EXTERNAL_HOSTNAME__/"${RENDER_EXTERNAL_HOSTNAME}"/g /app/apache2.conf
+sed -i s/__RENDER_EXTERNAL_HOSTNAME__/"${RENDER_EXTERNAL_HOSTNAME}"/g ./apache2.conf
 
 apachectl -f /app/apache2.conf -DFOREGROUND
--- app/start_pre.sh
+++ app/start_pre.sh
@@ -7,7 +7,8 @@ date +'%Y-%m-%d %H:%M:%S'
 apt-get -qq update
 DEBIAN_FRONTEND=noninteractive apt-get -q install -y --no-install-recommends curl iproute2 >/dev/null
 
-curl -sSL -H 'Cache-Control: no-cache' -o /app/start.sh https://raw.githubusercontent.com/tshr20180821/render-10/main/app/start.sh?$(date +%s)
-cat /app/start.sh
-chmod +x /app/start.sh
-/app/start.sh
+curl -sSL -H 'Cache-Control: no-cache' -O https://github.com/tshr20180821/render-10/raw/main/app/start.sh
+
+cat ./start.sh
+chmod +x ./start.sh
+./start.sh
