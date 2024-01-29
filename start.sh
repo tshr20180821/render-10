6d96ab3dc1f2d162a50d68cb25b8253f16e970b9
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-29 11:16:09 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,7 +20,7 @@ a2dissite -q 000-default.conf
 
 mkdir -p /var/www/html/auth
 
-curl -sSL -o /var/www/html/auth/distccd.php https://raw.githubusercontent.com/tshr20180821/render-10/main/distccd.php
+curl -sSL -o /var/www/html/auth/distccd.php https://github.com/tshr20180821/render-10/raw/main/distccd.php
 
 chown www-data:www-data /var/www/html/auth -R
 
