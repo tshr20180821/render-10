8607341aeb48a2993e261636b25fdd6018fe46c3
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-01-31 12:52:46 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -104,6 +104,8 @@ socat "exec:curl -u \"${BASIC_USER}:${BASIC_PASSWORD}\" -NsS https\://${RENDER_E
 # socat -d tcp-listen:8022,bind=127.0.0.1,reuseaddr,fork \
 #   "exec:curl -u \"${BASIC_USER}:${BASIC_PASSWORD}\" -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res | openssl aes-256-cbc -d -k ${PIPING_PASSWORD}!!exec:openssl aes-256-cbc -k ${PIPING_PASSWORD} | curl -u \"${BASIC_USER}:${BASIC_PASSWORD}\" -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req"
 
+openssl list-cipher-commands
+
 # apache start
 
 exec /usr/sbin/apache2 -DFOREGROUND
