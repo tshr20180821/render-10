5abec8c66d0d25253470e14021fae76c95d6a5b9
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-02 12:57:27 +0900

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -4,10 +4,16 @@ set -x
 
 while true
 do
-  echo start socat
+  echo start socat 1.1
   KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
   echo "KEYWORD : ${KEYWORD}"
-  socat "exec:curl --http1.1 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl --http1.1 -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
+  socat "exec:curl -v --http1.1 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl -v --http1.1 -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
     tcp:127.0.0.1:${TARGET_PORT}
-  echo finish socat
+  echo finish socat 1.1
+  echo start socat 2
+  KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
+  echo "KEYWORD : ${KEYWORD}"
+  socat "exec:curl -v -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl -v -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
+    tcp:127.0.0.1:${TARGET_PORT}
+  echo finish socat 2
 done
