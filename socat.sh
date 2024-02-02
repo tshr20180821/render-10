416dea6a804771c8ec02a4219edb363d1cc14d24
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-02 16:25:28 +0900

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -9,13 +9,13 @@ echo "KEYWORD : ${KEYWORD}"
 for i in {1..10}
 do
   echo start socat 1.1
-  # socat "exec:curl -v --http1.1 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl -v --http1.1 -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
+  # socat "exec:curl --http1.1 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl --http1.1 -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
   #   tcp:127.0.0.1:${TARGET_PORT}
   socat "exec:curl -v --http1.1 -NsS ${PIPING_SERVER}/${KEYWORD}req!!exec:curl -v --http1.1 -m 3600 -NsS --data-binary @- ${PIPING_SERVER}/${KEYWORD}res" \
     tcp:127.0.0.1:${TARGET_PORT}
   echo finish socat 1.1
   echo start socat 2
-  # socat "exec:curl -v -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl -v -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
+  # socat "exec:curl -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
   #   tcp:127.0.0.1:${TARGET_PORT}
   socat "exec:curl -v -NsS ${PIPING_SERVER}/piping/${KEYWORD}req!!exec:curl -v -m 3600 -NsS --data-binary @- ${PIPING_SERVER}/piping/${KEYWORD}res" \
     tcp:127.0.0.1:${TARGET_PORT}
