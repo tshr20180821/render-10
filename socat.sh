75b65e2c5ccfeaa7a4897d4e5adaf0cc0253246e
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-02 12:33:16 +0900

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -2,12 +2,11 @@
 
 set -x
 
-KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
-echo "KEYWORD : ${KEYWORD}"
-
 while true
 do
   echo start socat
+  KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
+  echo "KEYWORD : ${KEYWORD}"
   socat "exec:curl -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
     tcp:127.0.0.1:${TARGET_PORT}
   echo finish socat
