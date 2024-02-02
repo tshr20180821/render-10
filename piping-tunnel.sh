383515a58a2bbac67fe0abc053b7c5223b4fe842
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-02 15:06:59 +0900

    Update piping-tunnel.sh

--- piping-tunnel.sh
+++ piping-tunnel.sh
@@ -9,6 +9,7 @@ echo "PIPING_PASSWORD : ${PIPING_PASSWORD}"
 while true
 do
   echo start piping-tunnel
-  piping-tunnel server --verbose 5 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --header "Authorization: Basic ${AUTH}" --server https://${RENDER_EXTERNAL_HOSTNAME}/piping req res
+  # piping-tunnel server --verbose 5 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --header "Authorization: Basic ${AUTH}" --server https://${RENDER_EXTERNAL_HOSTNAME}/piping req res
+  piping-tunnel server --verbose 5 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --header "Authorization: Basic ${AUTH}" --server ${PIPING_SERVER} req res
   echo finish piping-tunnel
 done
