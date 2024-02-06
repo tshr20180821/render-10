813a9d9a3c0a92a58fb320bb6bd1bd90a9010c22
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-06 12:56:48 +0900

    Update piping-tunnel.sh

--- piping-tunnel.sh
+++ piping-tunnel.sh
@@ -9,7 +9,7 @@ echo "PIPING_PASSWORD : ${PIPING_PASSWORD}"
 for i in {1..10}
 do
   echo start piping-tunnel ${i}
-  piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --server https://piping.glitch.me/ ${PIPING_PASSWORD}req ${PIPING_PASSWORD}res
+  piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --server https://ppng.io/ ${PIPING_PASSWORD}req ${PIPING_PASSWORD}res
 
   echo start piping-tunnel ${i} ${PIPING_SERVER}
   piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --server ${PIPING_SERVER} req res
