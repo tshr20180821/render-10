545e1d390df7ff9b4c628391d0f7086a8d8e4d07
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-06 11:34:02 +0900

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -11,8 +11,6 @@ echo "KEYWORD : ${KEYWORD}"
 for i in {1..10}
 do
   echo start socat https://ppng.io/ ${i}
-  # socat "exec:curl -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
-  #   tcp:127.0.0.1:${TARGET_PORT}
   socat -ddd -v -4 "exec:curl -v -k -NsS https\://ppng.io/${KEYWORD}req!!exec:curl -v -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
     tcp4:127.0.0.1:${TARGET_PORT}
 
