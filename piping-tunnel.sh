4f99900b27219e1b5ea5d2b7ca632b76b4b224a9
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-07 14:28:44 +0900

    Update piping-tunnel.sh

--- piping-tunnel.sh
+++ piping-tunnel.sh
@@ -6,7 +6,7 @@ AUTH=$(echo -n "${BASIC_USER}:${BASIC_PASSWORD}" | base64)
 PIPING_PASSWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
 echo "PIPING_PASSWORD : ${PIPING_PASSWORD}"
 
-for i in {1..10}
+for i in {1..2}
 do
   # echo start piping-tunnel ${i}
   # piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --server https://ppng.io/ ${PIPING_PASSWORD}req ${PIPING_PASSWORD}res
