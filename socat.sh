c14f70268177db4d847b75a610b2a7e1a5496f7a
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-07 12:29:34 +0900

    Add files via upload

--- socat.sh
+++ socat.sh
@@ -8,24 +8,26 @@ PIPING_SERVER=$(echo ${PIPING_SERVER} | sed 's/:/\\:/')
 KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
 echo "KEYWORD : ${KEYWORD}"
 
+# socat -ddd -v -4 "exec:curl -v -k -NsS https\://ppng.io/${KEYWORD}req!!exec:curl -v -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
+#   tcp4:127.0.0.1:${TARGET_PORT} &
+
 for i in {1..10}
 do
-  echo start socat https://piping.glitch.me/ ${i}
-  socat -ddd -v -4 "exec:curl -v -k -NsS https\://piping.glitch.me/${KEYWORD}req!!exec:curl -v -k -m 3600 -NsST - https\://piping.glitch.me/${KEYWORD}res" \
-    tcp4:127.0.0.1:${TARGET_PORT}
-
-  echo socat -d tcp4-listen:8022,bind=127.0.0.1,reuseaddr,fork \\ \
-    \"exec:curl -NsS ${PIPING_SERVER}/${KEYWORD}res!!exec:curl -NsST - ${PIPING_SERVER}/${KEYWORD}req\"
+  # echo socat -d tcp4-listen:8022,bind=127.0.0.1,reuseaddr,fork \\ \
+  #   \"exec:curl -NsS ${PIPING_SERVER}/${KEYWORD}res!!exec:curl -NsST - ${PIPING_SERVER}/${KEYWORD}req\"
 
   echo start socat 1.1 ${i}
-  # socat "exec:curl --http1.1 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl --http1.1 -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
-  #   tcp:127.0.0.1:${TARGET_PORT}
-  socat -ddd -v -4 "exec:curl -v -k --http1.1 -NsS ${PIPING_SERVER}/${KEYWORD}req!!exec:curl -v -k --http1.1 -m 3600 -NsST - ${PIPING_SERVER}/${KEYWORD}res" \
-    tcp4:127.0.0.1:${TARGET_PORT}
+  socat -ddd -v -4 "exec:curl -vk --http1.1 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl -vk --http1.1 -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
+    tcp:127.0.0.1:${TARGET_PORT}
 
-  echo start socat 2 ${i}
+  curl -u ${BASIC_USER}\:${BASIC_PASSWORD} -d 'dummy' https://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req
+
+  # socat -ddd -v -4 "exec:curl -v -k --http1.1 -NsS ${PIPING_SERVER}/${KEYWORD}req!!exec:curl -v -k --http1.1 -m 3600 -NsST - ${PIPING_SERVER}/${KEYWORD}res" \
+  #  tcp4:127.0.0.1:${TARGET_PORT}
+
+  # echo start socat 2 ${i}
   # socat "exec:curl -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS --data-binary @- https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
   #   tcp:127.0.0.1:${TARGET_PORT}
-  socat -ddd -v -4 "exec:curl -v -k -NsS ${PIPING_SERVER}/${KEYWORD}req!!exec:curl -v -k -m 3600 -NsST - ${PIPING_SERVER}/${KEYWORD}res" \
-    tcp4:127.0.0.1:${TARGET_PORT}
+  # socat -ddd -v -4 "exec:curl -v -k -NsS ${PIPING_SERVER}/${KEYWORD}req!!exec:curl -v -k -m 3600 -NsST - ${PIPING_SERVER}/${KEYWORD}res" \
+  #   tcp4:127.0.0.1:${TARGET_PORT}
 done
