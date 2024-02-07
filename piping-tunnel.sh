5909fb37334359ffc05e25576ea0647e77a5c915
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-07 12:35:28 +0900

    Update piping-tunnel.sh

--- piping-tunnel.sh
+++ piping-tunnel.sh
@@ -8,12 +8,14 @@ echo "PIPING_PASSWORD : ${PIPING_PASSWORD}"
 
 for i in {1..10}
 do
-  echo start piping-tunnel ${i}
-  piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --server https://ppng.io/ ${PIPING_PASSWORD}req ${PIPING_PASSWORD}res
+  # echo start piping-tunnel ${i}
+  # piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --server https://ppng.io/ ${PIPING_PASSWORD}req ${PIPING_PASSWORD}res
 
-  echo start piping-tunnel ${i} ${PIPING_SERVER}
-  piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --server ${PIPING_SERVER} req res
+  # echo start piping-tunnel ${i} ${PIPING_SERVER}
+  # piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --server ${PIPING_SERVER} req res
 
   echo start piping-tunnel ${i} https://${RENDER_EXTERNAL_HOSTNAME}/piping
   piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --header "Authorization: Basic ${AUTH}" --server https://${RENDER_EXTERNAL_HOSTNAME}/piping req res
+
+  curl -u ${BASIC_USER}\:${BASIC_PASSWORD} -d 'dummy' https://${RENDER_EXTERNAL_HOSTNAME}/piping/req
 done
