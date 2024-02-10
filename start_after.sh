5ba0443609e567cc1f7b4b8f8351625ff16bf402
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-11 03:56:02 +0900

    Update start_after.sh

--- start_after.sh
+++ start_after.sh
@@ -101,6 +101,7 @@ ssh-keygen -f /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER} -t 
 
 ls -lang /home/${SSH_USER}/.ssh/
 
+sed -i 's/root/'${SSH_USER}'/' /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER}.pub
 cat /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER}.pub
 
 cat /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER}.pub >>/home/${SSH_USER}/.ssh/authorized_keys
