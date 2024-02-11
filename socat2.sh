c05f7160334ba6edcf001cac9ea5eac67c8fae42
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-12 08:31:04 +0900

    Create socat2.sh

---
+++ socat2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+export PS4='+(${BASH_SOURCE}:${LINENO}): '
+
+KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
+
+# socat -ddd -v -4 "exec:curl -v -k -NsS https\://ppng.io/${KEYWORD}req!!exec:curl -v -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
+#   tcp4:127.0.0.1:${TARGET_PORT}
+socat -4 "exec:curl -k -NsS https\://ppng.io/${KEYWORD}req!!exec:curl -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
+  tcp4:127.0.0.1:${TARGET_PORT}
