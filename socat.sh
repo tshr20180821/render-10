eb5d83cfe47506be1f2d059d4cdbab8098925921
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-11 12:24:20 +0900

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -10,8 +10,8 @@ echo "KEYWORD : ${KEYWORD}"
 
 # socat -ddd -v -4 "exec:curl -v -k -NsS https\://ppng.io/${KEYWORD}req!!exec:curl -v -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
 #   tcp4:127.0.0.1:${TARGET_PORT}
-socat -4 "exec:curl -k -NsS https\://ppng.io/${KEYWORD}req!!exec:curl -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
-  tcp4:127.0.0.1:${TARGET_PORT}
+# socat -4 "exec:curl -k -NsS https\://ppng.io/${KEYWORD}req!!exec:curl -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
+#   tcp4:127.0.0.1:${TARGET_PORT}
 # socat -ddd -v -4 "exec:curl -v -k -NsS https\://ppng.io/${KEYWORD}req!!exec:curl -v -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
 #   tcp4:127.0.0.1:8023
 
@@ -21,7 +21,9 @@ do
   #   \"exec:curl -NsS ${PIPING_SERVER}/${KEYWORD}res!!exec:curl -NsST - ${PIPING_SERVER}/${KEYWORD}req\"
 
   echo start socat 1.1 ${i}
-  socat -ddd -v -4 "exec:curl -vk --http1.1 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl -vk --http1.1 -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
+  # socat -ddd -v -4 "exec:curl -vk --http1.1 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl -vk --http1.1 -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
+  #   tcp:127.0.0.1:${TARGET_PORT}
+  socat -4 "exec:curl --http1.1 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsS https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req!!exec:curl --http1.1 -m 3600 -u ${BASIC_USER}\:${BASIC_PASSWORD} -NsST - https\://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}res" \
     tcp:127.0.0.1:${TARGET_PORT}
 
   curl -sSu ${BASIC_USER}:${BASIC_PASSWORD} -d 'dummy' https://${RENDER_EXTERNAL_HOSTNAME}/piping/${KEYWORD}req
