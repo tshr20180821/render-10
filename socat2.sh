87731ee21c3a0b7517d70e156494c776f082dbbf
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-12 08:32:48 +0900

    Update socat2.sh

--- socat2.sh
+++ socat2.sh
@@ -8,5 +8,5 @@ KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
 
 # socat -ddd -v -4 "exec:curl -v -k -NsS https\://ppng.io/${KEYWORD}req!!exec:curl -v -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
 #   tcp4:127.0.0.1:${TARGET_PORT}
-socat -4 "exec:curl -k -NsS https\://ppng.io/${KEYWORD}req!!exec:curl -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
+socat -4 "exec:curl -k -NsS https\://ppng.io/${KEYWORD}req | openssl aes-256-cbc!!exec:openssl aes-256-cbc -d | curl -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
   tcp4:127.0.0.1:${TARGET_PORT}
