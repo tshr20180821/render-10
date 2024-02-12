4b177a0a9908f63dacfdd82fc6aceb00cee47fa7
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-12 09:01:33 +0900

    Update socat2.sh

--- socat2.sh
+++ socat2.sh
@@ -6,7 +6,17 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): '
 
 KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
 
+{ \
+  echo "#!/bin/bash"; \
+  echo "";
+  echo "cat - | curl -NsS https://ppng.io/${KEYWORD}req"; \
+} >./tmp001.sh
+
+chmod +x ./tmp001.sh
+cat ./tmp001.sh
+pwd
+
 # socat -ddd -v -4 "exec:curl -v -k -NsS https\://ppng.io/${KEYWORD}req!!exec:curl -v -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
 #   tcp4:127.0.0.1:${TARGET_PORT}
-socat -4 "exec:curl -k -NsS https\://ppng.io/${KEYWORD}req | openssl aes-256-cbc!!exec:openssl aes-256-cbc -d | curl -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
+socat -4 "exec:./tmp001.sh!!exec:curl -k -m 3600 -NsST - https\://ppng.io/${KEYWORD}res" \
   tcp4:127.0.0.1:${TARGET_PORT}
