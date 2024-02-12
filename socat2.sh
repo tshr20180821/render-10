19c2bd5f85385c1b796cae87261de00b66be12e6
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-12 09:27:28 +0900

    Update socat2.sh

--- socat2.sh
+++ socat2.sh
@@ -7,6 +7,16 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): '
 KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
 
 { \
+  echo "curl -sSu ${BASIC_USER}:${BASIC_PASSWORD} https://${RENDER_EXTERNAL_HOSTNAME}/auth/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER} >key.txt"; \
+  echo "set +H"; \
+  echo "socat -4 tcp4-listen:8022,bind=127.0.0.1 'exec:curl -NsS https\://ppng.io/${KEYWORD}res!!exec:curl -NsST - https\://ppng.io/${KEYWORD}req' &"; \
+  echo "set -H"; \
+  echo "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l ${SSH_USER} -p 8022 127.0.0.1 -i ./key.txt"; \
+} >MESSAGE.txt
+
+cat MESSAGE.txt
+
+{ \
   echo "#!/bin/bash"; \
   echo "";
   echo "cat - | curl -NsS https://ppng.io/${KEYWORD}req"; \
