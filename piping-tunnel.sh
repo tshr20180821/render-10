1fc55368837bd3f812dcf8b5141d066ae7175657
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-16 15:38:30 +0900

    Add files via upload

--- piping-tunnel.sh
+++ piping-tunnel.sh
@@ -23,7 +23,7 @@ for i in {1..5}
 do
   echo start piping-tunnel ${i}
   # piping-tunnel server --verbose 5 --host 127.0.0.1 --port ${TARGET_PORT} --symmetric --server https://ppng.io ${KEYWORD}req ${KEYWORD}res
-  piping-tunnel server --verbose 5 --host 127.0.0.1 --port ${TARGET_PORT} --server https://ppng.io --cipher-type=aes-256-ctr --symmetric --pass ${PIPING_PASSWORD} ${KEYWORD}req ${KEYWORD}res
+  piping-tunnel server --verbose 5 --host 127.0.0.1 --port ${TARGET_PORT} --server https://ppng.io --cipher-type=openssl-aes-256-ctr --symmetric --pass ${PIPING_PASSWORD} ${KEYWORD}req ${KEYWORD}res
 
   # echo start piping-tunnel ${i} ${PIPING_SERVER}
   # piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --server ${PIPING_SERVER} req res
