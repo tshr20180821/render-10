95dd176e43f0ea27e06b4799b9b0b3dc097b658d
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-16 13:28:18 +0900

    Update fah.sh

--- fah.sh
+++ fah.sh
@@ -6,6 +6,7 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): '
 
 curl -sSO https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/fahclient_7.6.21_amd64.deb
 
+mkdir -p /usr/share/doc/fahclient
 touch /usr/share/doc/fahclient/sample-config.xml
 
 DEBIAN_FRONTEND=noninteractive apt-get -y install ./fahclient_7.6.21_amd64.deb
@@ -15,4 +16,4 @@ find / -name FAHClient -print 2>/dev/null
 FAHClient --help
 
 FAHClient -v --user=Anonymous --team=0 --gpu=false --gui-enabled=false --cpus=$(nproc) --chdir=/tmp --log-to-screen=true \
-  --http-addresses=127.0.0.1:7396 --command-address=127.0.0.1 --max-packet-size=small --priority=high --verbosity=5 &
+  --http-addresses=127.0.0.1:7396 --command-address=127.0.0.1 --max-packet-size=small --priority=normal --verbosity=5 &
