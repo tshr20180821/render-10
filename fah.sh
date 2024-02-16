84e6dd8d33226e15c9c61f37f040209f32e02df8
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-16 10:23:31 +0900

    Update fah.sh

--- fah.sh
+++ fah.sh
@@ -6,7 +6,7 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): '
 
 curl -sSO https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/fahclient_7.6.21_amd64.deb
 
-dpkg -i fahclient_7.6.21_amd64.deb
+DEBIAN_FRONTEND=noninteractive apt-get -y install ./fahclient_7.6.21_amd64.deb
 
 find / -name fahclient -print 2>/dev/null
 
