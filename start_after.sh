fb9398e03fb8d455109ff67b8b1a444537803578
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-16 16:24:51 +0900

    Update start_after.sh

--- start_after.sh
+++ start_after.sh
@@ -84,6 +84,7 @@ ls -lang /home/${SSH_USER}/.ssh/
 
 mkdir -p /root/.ssh
 cp /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER}.pub /root/.ssh/
+cat /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER}.pub >>/etc/dropbear/authorized_keys
 
 sed -i 's/root/'${SSH_USER}'/' /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER}.pub
 cat /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER}.pub >>/etc/dropbear/authorized_keys
