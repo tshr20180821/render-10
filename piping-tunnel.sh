022615389d329405e4dd8f3da421f57637169a32
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-16 13:58:31 +0900

    Update piping-tunnel.sh

--- piping-tunnel.sh
+++ piping-tunnel.sh
@@ -8,7 +8,7 @@ curl -sSLO https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.10.2/
 dpkg -i piping-tunnel-0.10.2-linux-amd64.deb
 
 # AUTH=$(echo -n "${BASIC_USER}:${BASIC_PASSWORD}" | base64)
-# PIPING_PASSWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
+PIPING_PASSWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
 # echo "PIPING_PASSWORD : ${PIPING_PASSWORD}"
 
 KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
@@ -19,7 +19,7 @@ for i in {1..5}
 do
   echo start piping-tunnel ${i}
   # piping-tunnel server --verbose 5 --host 127.0.0.1 --port ${TARGET_PORT} --symmetric --server https://ppng.io ${KEYWORD}req ${KEYWORD}res
-  piping-tunnel server --verbose 5 --host 127.0.0.1 --port ${TARGET_PORT} --server https://ppng.io --symmetric --pass ${KEYWORD} ${KEYWORD}req ${KEYWORD}res
+  piping-tunnel server --verbose 5 --host 127.0.0.1 --port ${TARGET_PORT} --server https://ppng.io --symmetric --pass ${PIPING_PASSWORD} ${KEYWORD}req ${KEYWORD}res
 
   # echo start piping-tunnel ${i} ${PIPING_SERVER}
   # piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --server ${PIPING_SERVER} req res
