dbf45b4872e9c8415597686cce36f4fe56d06f44
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-18 06:16:59 +0900

    Update socat3.sh

--- socat3.sh
+++ socat3.sh
@@ -36,7 +36,7 @@ do
   
   curl ${AUTH} -sSN ${PIPING_SERVER}/${KEYWORD}req \
    | stdbuf -i0 -o0 openssl aes-256-ctr -d -pass "pass:${PASSWORD}" -bufsize 1 -pbkdf2 -iter 1000 -md sha256 \
-   | socat - tcp4:127.0.0.1:${TARGET_PORT} \
+   | socat tcp4:127.0.0.1:${TARGET_PORT} - \
    | stdbuf -i0 -o0 openssl aes-256-ctr -pass "pass:${PASSWORD}" -bufsize 1 -pbkdf2 -iter 1000 -md sha256 \
    | curl ${AUTH} -m 300 -sSNT - ${PIPING_SERVER}/${KEYWORD}res
 done
