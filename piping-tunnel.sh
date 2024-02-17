6bea8cb0452380d9a4463b82ded7486abc92f8b4
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-17 22:49:15 +0900

    Update piping-tunnel.sh

--- piping-tunnel.sh
+++ piping-tunnel.sh
@@ -15,7 +15,7 @@ rm ./fahclient_7.6.21_amd64.deb
 curl -sSLO https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.10.2/piping-tunnel-0.10.2-linux-amd64.deb
 dpkg -i piping-tunnel-0.10.2-linux-amd64.deb
 
-PIPING_SERVER=https://ppng.io
+# PIPING_SERVER=https://ppng.io
 
 # AUTH=$(echo -n "${BASIC_USER}:${BASIC_PASSWORD}" | base64)
 PIPING_PASSWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
@@ -45,14 +45,6 @@ curl -sS -X POST -H "Authorization: Bearer ${SLACK_TOKEN}" -H "Content-Type: app
 for i in {1..5}
 do
   echo start piping-tunnel ${i}
-  # piping-tunnel server --verbose 5 --host 127.0.0.1 --port ${TARGET_PORT} --symmetric --server https://ppng.io ${KEYWORD}req ${KEYWORD}res
-  piping-tunnel server --verbose 5 --host 127.0.0.1 --port ${TARGET_PORT} --server https://ppng.io --cipher-type=openssl-aes-256-ctr --pbkdf2='{"iter":1000,"hash":"sha256"}' --symmetric --pass ${PIPING_PASSWORD} ${KEYWORD}req ${KEYWORD}res
-
-  # echo start piping-tunnel ${i} ${PIPING_SERVER}
-  # piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --server ${PIPING_SERVER} req res
-
-  # echo start piping-tunnel ${i} https://${RENDER_EXTERNAL_HOSTNAME}/piping
-  # piping-tunnel server --verbose 5 --host 127.0.0.1 --pass ${PIPING_PASSWORD} --port ${TARGET_PORT} --symmetric --header "Authorization: Basic ${AUTH}" # --server https://${RENDER_EXTERNAL_HOSTNAME}/piping req res
-
-  # curl -sSu ${BASIC_USER}:${BASIC_PASSWORD} -d 'dummy' https://${RENDER_EXTERNAL_HOSTNAME}/piping/req
+  # piping-tunnel server --verbose 5 --host 127.0.0.1 --port ${TARGET_PORT} --server https://ppng.io --cipher-type=openssl-aes-256-ctr --pbkdf2='{"iter":1000,"hash":"sha256"}' --symmetric --pass ${PIPING_PASSWORD} ${KEYWORD}req ${KEYWORD}res
+  piping-tunnel server --host 127.0.0.1 --port ${TARGET_PORT} --cipher-type=openssl-aes-256-ctr --pbkdf2='{"iter":1000,"hash":"sha256"}' --symmetric --pass ${PIPING_PASSWORD} ${KEYWORD}req ${KEYWORD}res
 done
