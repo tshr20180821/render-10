84afa66d13c984f414de0baafb294ed4857863b5
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-17 21:12:13 +0900

    Update start_after.sh

--- start_after.sh
+++ start_after.sh
@@ -109,8 +109,8 @@ sleep 3s
 
 # curl -sSL -O https://github.com/tshr20180821/render-10/raw/main/socat.sh
 # curl -sSL -O https://github.com/tshr20180821/render-10/raw/main/socat2.sh
-# curl -sSL -O https://github.com/tshr20180821/render-10/raw/main/socat3.sh
-curl -sSL -O https://github.com/tshr20180821/render-10/raw/main/piping-tunnel.sh
+curl -sSL -O https://github.com/tshr20180821/render-10/raw/main/socat3.sh
+# curl -sSL -O https://github.com/tshr20180821/render-10/raw/main/piping-tunnel.sh
 # curl -sSL -O https://github.com/tshr20180821/render-10/raw/main/piping-duplex.sh
 # curl -sSL -O https://github.com/tshr20180821/render-10/raw/main/fah.sh
 
