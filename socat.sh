8da82442913cc66e67a0c8fb06f7cb72dd06d95e
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-20 15:45:44 +0900

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -15,7 +15,7 @@ echo "KEYWORD : ${KEYWORD}"
   echo "curl -sSu ${BASIC_USER}:${BASIC_PASSWORD} https://${RENDER_EXTERNAL_HOSTNAME}/auth/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER} >key.txt"; \
   echo "chmod 600 key.txt"; \
   echo "set +H"; \
-  echo "socat -4 tcp4-listen:8022,bind=127.0.0.1 'exec:curl ${AUTH} -NsS ${PIPING_SERVER}/${KEYWORD}res!!exec:curl ${AUTH} -NsST - ${PIPING_SERVER}/${KEYWORD}req' &"; \
+  echo "socat -4 tcp4-listen:8022,bind=127.0.0.1 'exec:curl ${AUTH} --http1.1 -NsS ${PIPING_SERVER}/${KEYWORD}res!!exec:curl ${AUTH} --http1.1 -NsST - ${PIPING_SERVER}/${KEYWORD}req' &"; \
   echo "set -H"; \
   echo "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l ${SSH_USER} -p 8022 127.0.0.1 -i ./key.txt"; \
 } >MESSAGE.txt
@@ -24,13 +24,13 @@ MESSAGE=$(cat MESSAGE.txt | base64 -w 0)
 rm MESSAGE.txt
 
 curl -sS -X POST -H "Authorization: Bearer ${SLACK_TOKEN}" -H "Content-Type: application/json" \
-  -d "{\"channel\":\"${SLACK_CHANNEL}\",\"text\":\"${MESSAGE}\"}" https://slack.com/api/chat.postMessage
+  -d "{\"channel\":\"${SLACK_CHANNEL}\",\"text\":\"${MESSAGE}\"}" https://slack.com/api/chat.postMessage >/dev/null
 
 for i in {1..2}
 do
   echo start socat ${i}
   # NG
-  socat "exec:curl ${AUTH}  -NsS ${PIPING_SERVER}/${KEYWORD}req!!exec:curl -m 3600 ${AUTH} -NsS --data-binary @- ${PIPING_SERVER}/${KEYWORD}res" \
+  socat "exec:curl ${AUTH} --http1.1 -NsS ${PIPING_SERVER}/${KEYWORD}req!!exec:curl -m 3600 ${AUTH} --http1.1 -NsS --data-binary @- ${PIPING_SERVER}/${KEYWORD}res" \
     tcp4:127.0.0.1:${TARGET_PORT}
   # OK
   # socat -4 "exec:curl -NsS ${PIPING_SERVER}/${KEYWORD}req!!exec:curl -m 3600 -NsST - ${PIPING_SERVER}/${KEYWORD}res" \
