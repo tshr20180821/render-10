c2201619e639b013f31b2e6f34baf1097d1cebc6
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-20 10:19:06 +0900

    Update piping-tunnel.sh

--- piping-tunnel.sh
+++ piping-tunnel.sh
@@ -4,10 +4,12 @@ set -x
 
 export PS4='+(${BASH_SOURCE}:${LINENO}): '
 
-curl -sSLo dropbox.deb https://www.dropbox.com/download?dl=packages/debian/dropbox_2024.01.22_amd64.deb
+# curl -sSLo dropbox.deb https://www.dropbox.com/download?dl=packages/debian/dropbox_2024.01.22_amd64.deb
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ./dropbox.deb
-rm dropbox.deb
+# DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ./dropbox.deb
+# rm dropbox.deb
+
+curl -sSL https://www.dropbox.com/download?plat=lnx.x86_64 | tar xzf -
 
 find / -name dropboxd -print 2>/dev/null
 
