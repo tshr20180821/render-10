20afc7b6bddb011774f6ff2f4efb178b8f02e069
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-22 10:21:43 +0900

    Update megatools.sh

--- megatools.sh
+++ megatools.sh
@@ -23,4 +23,4 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
 
 megatools --help
 megatools reg --help
-megatools reg -e ${MEGA_EMAIL} -p ${MEGA_PASSWORD} --register
+megatools reg -e ${MEGA_EMAIL} -p ${MEGA_PASSWORD} -n ${MEGA_NAME} --register
