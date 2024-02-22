a07e8029ab8b02defd21840469023d117bdece49
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-22 11:02:27 +0900

    Update megatools.sh

--- megatools.sh
+++ megatools.sh
@@ -4,19 +4,10 @@ set -x
 
 export PS4='+(${BASH_SOURCE}:${LINENO}): '
 
-# curl -sSLo dropbox.deb https://www.dropbox.com/download?dl=packages/debian/dropbox_2024.01.22_amd64.deb
-# DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ./dropbox.deb
-# rm dropbox.deb
+curl -sSO https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/latest.deb
 
-# curl -sSL https://www.dropbox.com/download?plat=lnx.x86_64 | tar xzf -
-# find / -name dropboxd -print 2>/dev/null
-# /usr/src/app/.dropbox-dist/dropboxd --help
-# cat /tmp/dropbox_error*.txt
-
-# curl -sSLO https://github.com/rclone/rclone/releases/download/v1.65.2/rclone-v1.65.2-linux-amd64.deb
-# dpkg -i rclone-v1.65.2-linux-amd64.deb
-# rm rclone-v1.65.2-linux-amd64.deb
-# rclone --help
+DEBIAN_FRONTEND=noninteractive apt-get -y install ./latest.deb
+rm ./latest.deb
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
   megatools
@@ -25,17 +16,13 @@ echo "[Login]" >/root/.megarc
 echo "Username = ${MEGA_EMAIL}" >>/root/.megarc
 echo "Password = ${MEGA_PASSWORD}" >>/root/.megarc
 
-megatools --help
-megatools df --help
-megatools ls --help
-megatools mkdir --help
 megatools mkdir /Root/${RENDER_EXTERNAL_HOSTNAME}
 megatools ls -l /Root/${RENDER_EXTERNAL_HOSTNAME}
 
 echo "DUMMY" >./dummy.txt
 
 megatools put --path /Root/${RENDER_EXTERNAL_HOSTNAME}/ dummy.txt
-megatools ls -l /Root/${RENDER_EXTERNAL_HOSTNAME}
+megatools ls -l /Root/${RENDER_EXTERNAL_HOSTNAME}/
 
 rm ./dummy.txt
 
