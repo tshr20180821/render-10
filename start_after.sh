ed663221ccafb692bee83eb8f9fa082bebe1ce17
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-28 11:53:32 +0900

    Update start_after.sh

--- start_after.sh
+++ start_after.sh
@@ -82,14 +82,13 @@ service telnet
   wait            = no
   user            = root
   server          = /usr/sbin/telnetd
-  log_on_failure  += USERID
+  log_on_success  = HOST PID USERID
+  log_on_failure  = HOST USERID
   disable         = no
   server_args     = -h -a valid
   bind            = 127.0.0.1
   only_from       = 127.0.0.1
   log_type        = FILE /var/log/xinetd.log
-  log_on_success  = HOST PID
-  log_on_failure  = HOST
 }
 EOF
 
