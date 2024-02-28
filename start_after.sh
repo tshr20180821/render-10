134d283c3561dcfa02b33744e0530ab3dbdcc428
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-28 10:24:16 +0900

    Update start_after.sh

--- start_after.sh
+++ start_after.sh
@@ -49,7 +49,8 @@ DEBIAN_FRONTEND=noninteractive apt-fast install -y --no-install-recommends \
   socat \
   systemctl \
   telnetd \
-  vim &
+  vim \
+  xinetd &
 
 # ROOT_PASSWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
 export SSH_USER=$(tr -dc 'a-z' </dev/urandom | fold -w 16 | head -n 1)
