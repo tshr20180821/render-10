3c3d5b5e049dca27fd21625a16a57903a35efaf4
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-03-03 06:58:24 +0900

    Update start_after.sh

--- start_after.sh
+++ start_after.sh
@@ -10,7 +10,7 @@ curl -sSLo /usr/local/sbin/apt-fast https://raw.githubusercontent.com/ilikenwf/a
 chmod +x /usr/local/sbin/apt-fast
 
 # echo "MIRRORS=('http://deb.debian.org/debian','http://ftp.debian.org/debian,http://ftp2.de.debian.org/debian,http://ftp.de.debian.org/debian,ftp://ftp.uni-kl.de/debian')" >/etc/apt-fast.conf
-echo "MIRRORS=('http://deb.debian.org/debian','http://ftp.debian.org/debian,http://mirror.coganng.com/debian/,http://mirror.sg.gs/debian/,http://ossmirror.mycloud.services/debian/')" >/etc/apt-fast.conf
+echo "MIRRORS=('http://deb.debian.org/debian','http://ftp.debian.org/debian,http://mirror.coganng.com/debian/,http://mirror.sg.gs/debian/,http://ossmirror.mycloud.services/debian/,http://ftp.nara.wide.ad.jp/debian/,http://ftp.kddilabs.jp/pub/debian/,http://ftp.riken.jp/Linux/debian/debian/')" >/etc/apt-fast.conf
 
 DEBIAN_FRONTEND=noninteractive apt-get -qq install -y --no-install-recommends \
   aria2 \
