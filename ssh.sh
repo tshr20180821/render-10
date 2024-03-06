fb5a61b315ef45787d85888df30a0da01425ec1d
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-03-06 09:38:24 +0900

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -7,4 +7,4 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): '
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
   -p ${TARGET_PORT} \
   -i /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER} \
-  -fNL 13632:127.0.0.1:3632 ${SSH_USER}@127.0.0.1
+  -4fNL 13632:127.0.0.1:3632 ${SSH_USER}@127.0.0.1
