b030b55b1e6d94712654af0f46b478b95d2e3b1a
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-03-06 11:45:43 +0900

    Add files via upload

--- socat4.sh
+++ socat4.sh
@@ -9,26 +9,32 @@ PIPING_SERVER=https://ppng.io
 PASSWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)
 KEYWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 64 | head -n 1)
 
-curl -sSN ${PIPING_SERVER}/${KEYWORD}req \
-  | stdbuf -i0 -o0 openssl aes-256-ctr -d -pass "pass:${PASSWORD}" -bufsize 1 -pbkdf2 -iter 1000 -md sha256 \
-  | socat - tcp4:127.0.0.1:${TARGET_PORT} \
-  | stdbuf -i0 -o0 openssl aes-256-ctr -pass "pass:${PASSWORD}" -bufsize 1 -pbkdf2 -iter 1000 -md sha256 \
-  | curl -m 3600 -sSNT - ${PIPING_SERVER}/${KEYWORD}res &
+while true; do \
+  curl -sSN ${PIPING_SERVER}/${KEYWORD}req \
+    | stdbuf -i0 -o0 openssl aes-256-ctr -d -pass "pass:${PASSWORD}" -bufsize 1 -pbkdf2 -iter 1000 -md sha256 \
+    | socat - tcp4:127.0.0.1:${TARGET_PORT} \
+    | stdbuf -i0 -o0 openssl aes-256-ctr -pass "pass:${PASSWORD}" -bufsize 1 -pbkdf2 -iter 1000 -md sha256 \
+    | curl -m 3600 -sSNT - ${PIPING_SERVER}/${KEYWORD}res; \
+done &
 
-curl -NsSL ${PIPING_SERVER}/${KEYWORD}res \
-  | stdbuf -i0 -o0 openssl aes-256-ctr -d -pass "pass:${PASSWORD}" -bufsize 1 -pbkdf2 -iter 1000 -md sha256 \
-  | socat tcp4-listen:9022,bind=127.0.0.1 - \
-  | stdbuf -i0 -o0 openssl aes-256-ctr -pass "pass:${PASSWORD}" -bufsize 1 -pbkdf2 -iter 1000 -md sha256 \
-  | curl -m 3600 -NsSLT - ${PIPING_SERVER}/${KEYWORD}req &
+while true; do \
+  curl -NsSL ${PIPING_SERVER}/${KEYWORD}res \
+    | stdbuf -i0 -o0 openssl aes-256-ctr -d -pass "pass:${PASSWORD}" -bufsize 1 -pbkdf2 -iter 1000 -md sha256 \
+    | socat tcp4-listen:9022,bind=127.0.0.1,reuseaddr - \
+    | stdbuf -i0 -o0 openssl aes-256-ctr -pass "pass:${PASSWORD}" -bufsize 1 -pbkdf2 -iter 1000 -md sha256 \
+    | curl -m 3600 -NsSLT - ${PIPING_SERVER}/${KEYWORD}req;
+done &
 
 sleep 3s
 
 ssh --help
 
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-  -o ServerAliveInterval=60 -o ServerAliveCountMax=60 \
-  -i /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER} \
-  -4fNL 13632:127.0.0.1:3632 ${SSH_USER}@127.0.0.1:9022
+while true; do \
+  ssh -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+    -o ServerAliveInterval=60 -o ServerAliveCountMax=60 \
+    -i /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER} \
+    -4nNL 13632:127.0.0.1:3632 ${SSH_USER}@127.0.0.1:9022;
+done &
 
 # memcached
 
