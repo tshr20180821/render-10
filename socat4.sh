5da1918d3cbd2764b577800f7c2f6636170a264b
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-03-06 10:20:53 +0900

    Update socat4.sh

--- socat4.sh
+++ socat4.sh
@@ -27,3 +27,32 @@ ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
   -p 9022 \
   -i /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER} \
   -4fNL 13632:127.0.0.1:3632 ${SSH_USER}@127.0.0.1
+
+# memcached
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y libevent-dev >/dev/null 2>&1
+
+gcc -### -E - -march=native 2>&1 | sed -r '/cc1/!d;s/(")|(^.* - )//g' >/tmp/cflags_option
+cflags_option=$(cat /tmp/cflags_option)
+export CFLAGS="-O2 ${cflags_option} -pipe -fomit-frame-pointer"
+export CXXFLAGS="${CFLAGS}"
+export LDFLAGS="-fuse-ld=gold"
+
+pushd /tmp
+curl -sSO https://memcached.org/files/memcached-1.6.22.tar.gz
+tar xf memcached-1.6.22.tar.gz
+
+export DISTCC_HOSTS="127.0.0.1:13632/4"
+export DISTCC_POTENTIAL_HOSTS="${DISTCC_HOSTS}"
+export DISTCC_FALLBACK=0
+
+pushd memcached-1.6.22
+
+./configure --disable-docs >/dev/null
+
+time HOME=/tmp MAKEFLAGS="CC=distcc\ gcc" make -j4
+
+popd
+popd
+
+ls -lang /var/www/html/auth/
