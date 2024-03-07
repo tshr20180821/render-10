a803e69b6e3f04852a700892b3e1283ce9ff0bff
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-03-07 10:37:52 +0900

    Update socat4.sh

--- socat4.sh
+++ socat4.sh
@@ -35,7 +35,7 @@ ssh -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
   -o ServerAliveInterval=60 -o ServerAliveCountMax=60 \
   -p 9022 \
   -i /home/${SSH_USER}/.ssh/${RENDER_EXTERNAL_HOSTNAME}-${SSH_USER} \
-  -4fNL 13632:127.0.0.1:3632 ${SSH_USER}@127.0.0.1
+  -4fNL 13632:127.0.0.1:3632 ${SSH_USER}@127.0.0.1 &
 
 # memcached
 
