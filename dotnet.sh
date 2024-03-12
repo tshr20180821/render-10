af9ae1e79a3af95db68b56756e9c58cc123fb717
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-03-12 13:01:53 +0900

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -8,9 +8,9 @@ curl -sSLO https://packages.microsoft.com/config/debian/12/packages-microsoft-pr
 dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
-apt-fast -qq update
+apt-get -qq update
 
-DEBIAN_FRONTEND=noninteractive apt-fast install -y --no-install-recommends dotnet-sdk-8.0
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends dotnet-sdk-8.0
 
 dotnet --help
 
